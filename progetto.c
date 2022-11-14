39e054e42684e00fe307d24513da8b45b5a90d5c
BidoiaSimone <117077053+BidoiaSimone@users.noreply.github.com>
2022-11-14 18:51:02 +0100

    progetto

--- progetto.c
+++ progetto.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define WALL    '#'
 
 
 void labyrinth_initializer(char **M){
@@ -17,17 +19,21 @@ void labyrinth_initializer(char **M){
     for(int i = 0; i < row; i++){
         for(int j = 0; j < col; j++){           //assegna '#' ai muri esterni del labirinto
             if(i == 0 || i == row - 1){         //sopra-sotto
+                M[i][j] = WALL;
             }
             if(j == 0 || j == col - 1){         //sinistra-destra
+                M[i][j] = WALL;
             }
         }
     }
 
     for(int i = 1; i <= row - 4; i++){          //crea primo separatore
+        M[i][5] = WALL;
     }
 
 
     for(int i = row - 1; i >= row - 7; i--){    //crea secondo separatore
+        M[i][12] = WALL;
     }
 
 
@@ -50,6 +56,7 @@ void labyrinth_initializer(char **M){
 
 
 void labyrinth_player(char **M,int *row,int *col){
+    char c = '\0';
     int g_row = 1;
     int g_col = 0;
     int points = 112;
@@ -67,6 +74,7 @@ void labyrinth_player(char **M,int *row,int *col){
     while((c != 'q')){          //muove pedina
         system("stty raw");
         c = getchar();
+        if(c == 'w' && M[g_row-1][g_col] != WALL && g_row-1 >= 0){           //muove pedina in su
             if(M[g_row-1][g_col] == '$')
                 points+=3;
             if(M[g_row-1][g_col] == '!')
@@ -76,6 +84,7 @@ void labyrinth_player(char **M,int *row,int *col){
             g_row--;
             points--;
         }
+        if(c == 's' && M[g_row+1][g_col] != WALL && g_row+1 >= 0){           //uove pedina in giù
             if(M[g_row+1][g_col] == '$')
                 points+=3;
             if(M[g_row+1][g_col] == '!')
@@ -85,6 +94,7 @@ void labyrinth_player(char **M,int *row,int *col){
             g_row++;
             points--;
         }
+        if(c == 'a' && M[g_row][g_col-1] != WALL && g_col-1 >= 0){           //muove pedina a sinistra
             if(M[g_row][g_col-1] == '$')
                 points+=3;
             if(M[g_row][g_col-1] == '!')
@@ -94,6 +104,7 @@ void labyrinth_player(char **M,int *row,int *col){
             g_col--;
             points--;
         }
+        if(c == 'd' && M[g_row][g_col+1] != WALL && g_col+1 >= 0){           //muove pedina a destra
             if(M[g_row][g_col+1] == '$')
                 points+=3;
             if(M[g_row][g_col+1] == '!')
@@ -114,9 +125,19 @@ void labyrinth_player(char **M,int *row,int *col){
                 }
                 printf("\n");
             }
+            
             if(points < 0)
+                points = 0;
+
+
+            if(points > 100)
+                printf("Score: 100\n");
+            else
+                printf("Score: %d\n", points);
+
         }
         if(points < 0)
+           points = 0;
         if(M[5][18] == 'o'){
             printf("Vittoria!!!\n");
             
@@ -126,7 +147,14 @@ void labyrinth_player(char **M,int *row,int *col){
   
 }
 
+int main(int argc, char * argv[]){
+//891868 895662
 
+if(argc == 2 && strcmp(argv[1], "--challenge") == 0){       //cambia comportamento del main se viene 
+    printf("%d\n%s\n", argc, argv[1]);                      //parametro "--challenge" in esecuzione
+    return 0;                                               //qui ora al posto della printf(test)
+                                                            //basta inserire la funzione che gestisce la IA
+}
     int row = 10;
     int col = 19;
     char **M = (char**) malloc(row * sizeof(char*));
