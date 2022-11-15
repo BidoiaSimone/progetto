6c1ce786d15c19c880070179d20222bc63df89c7
BidoiaSimone <117077053+BidoiaSimone@users.noreply.github.com>
2022-11-15 19:52:48 +0100

    progetto

--- progetto.c
+++ progetto.c
@@ -2,7 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define WALL    '#'
+
+
+
+void matrix_reader(char **M,int *row,int *col){
+        int i = 0;
+        int j = 0;
+        char c = '\0';
+        while(i < *row){
+            c = getchar();
+            if(c != '\n'){
+                M[i][j] = c;
+                j++;
+            }
+            if(c == '\n'){
+                i++;
+                j = 0;
+            }
+        }
+}
+
 
 
 void labyrinth_initializer(char **M){
@@ -19,21 +38,21 @@ void labyrinth_initializer(char **M){
     for(int i = 0; i < row; i++){
         for(int j = 0; j < col; j++){           //assegna '#' ai muri esterni del labirinto
             if(i == 0 || i == row - 1){         //sopra-sotto
-                M[i][j] = WALL;
+                M[i][j] = '#';
             }
             if(j == 0 || j == col - 1){         //sinistra-destra
-                M[i][j] = WALL;
+                M[i][j] = '#';
             }
         }
     }
 
     for(int i = 1; i <= row - 4; i++){          //crea primo separatore
-        M[i][5] = WALL;
+        M[i][5] = '#';
     }
 
 
     for(int i = row - 1; i >= row - 7; i--){    //crea secondo separatore
-        M[i][12] = WALL;
+        M[i][12] = '#';
     }
 
 
@@ -54,27 +73,42 @@ void labyrinth_initializer(char **M){
 
 
 
-
-void labyrinth_player(char **M,int *row,int *col){
+void labyrinth_player(char **M, int *row, int *col){
     char c = '\0';
-    int g_row = 1;
-    int g_col = 0;
+    int g_col;
+    int g_row;
+    int victory_row;
+    int victory_col;
     int points = 112;
-    M[g_row][g_col] = 'o';  //giocatore
+    for(int i = 0; i < *row; i++){
+        for(int j = 0; j < *col; j++){          //controlla dove è la posizione di partenza del giocaotore
+            if(M[i][j] == 'o'){
+                g_col = j;
+                g_row = i;
+            }
+            if(M[i][j] == '_'){
+                victory_row = i;
+                victory_col = j;
+            }
+        }
+    }
 
 
     system("clear");
-    for(int i = 0; i < *row; i++){
-        for(int j = 0; j < *col; j++){              //stampa labirinto di partenza
+    for(int i = 0; i < *row; i++){                   //stampa matrice iniziale
+        for(int j = 0; j < *col; j++){      
             printf("%c", M[i][j]);
         }
+            printf("\n");
+    }   
         printf("\n");
-    }
     
+
+
     while((c != 'q')){          //muove pedina
         system("stty raw");
         c = getchar();
-        if(c == 'w' && M[g_row-1][g_col] != WALL && g_row-1 >= 0){           //muove pedina in su
+        if(c == 'w' && M[g_row-1][g_col] != '#' && g_row-1 >= 0){           //muove pedina in su
             if(M[g_row-1][g_col] == '$')
                 points+=3;
             if(M[g_row-1][g_col] == '!')
@@ -84,7 +118,7 @@ void labyrinth_player(char **M,int *row,int *col){
             g_row--;
             points--;
         }
-        if(c == 's' && M[g_row+1][g_col] != WALL && g_row+1 >= 0){           //uove pedina in giù
+        if(c == 's' && M[g_row+1][g_col] != '#' && g_row+1 >= 0){           //uove pedina in giù
             if(M[g_row+1][g_col] == '$')
                 points+=3;
             if(M[g_row+1][g_col] == '!')
@@ -94,7 +128,7 @@ void labyrinth_player(char **M,int *row,int *col){
             g_row++;
             points--;
         }
-        if(c == 'a' && M[g_row][g_col-1] != WALL && g_col-1 >= 0){           //muove pedina a sinistra
+        if(c == 'a' && M[g_row][g_col-1] != '#' && g_col-1 >= 0){           //muove pedina a sinistra
             if(M[g_row][g_col-1] == '$')
                 points+=3;
             if(M[g_row][g_col-1] == '!')
@@ -104,7 +138,7 @@ void labyrinth_player(char **M,int *row,int *col){
             g_col--;
             points--;
         }
-        if(c == 'd' && M[g_row][g_col+1] != WALL && g_col+1 >= 0){           //muove pedina a destra
+        if(c == 'd' && M[g_row][g_col+1] != '#' && g_col+1 >= 0){           //muove pedina a destra
             if(M[g_row][g_col+1] == '$')
                 points+=3;
             if(M[g_row][g_col+1] == '!')
@@ -119,13 +153,14 @@ void labyrinth_player(char **M,int *row,int *col){
         
         if(c != '\n'){              //stampa labirinto aggiornato di 1 mossa
             system("clear");
-            for(int i = 0; i < *row; i++){
-                for(int j = 0; j < *col; j++){       
-                    printf("%c", M[i][j]);
+            for(int i = 0; i < *row; i++){                   //stampa matrice aggiornata di 1 mossa
+                for(int j = 0; j < *col; j++){      
+                        printf("%c", M[i][j]);
                 }
                 printf("\n");
-            }
-            
+            }   
+        printf("\n");
+        }
             if(points < 0)
                 points = 0;
 
@@ -135,10 +170,10 @@ void labyrinth_player(char **M,int *row,int *col){
             else
                 printf("Score: %d\n", points);
 
-        }
+        
         if(points < 0)
            points = 0;
-        if(M[5][18] == 'o'){
+        if(M[victory_row][victory_col] == 'o'){
             printf("Vittoria!!!\n");
             
             break;
@@ -147,25 +182,46 @@ void labyrinth_player(char **M,int *row,int *col){
   
 }
 
+
+
 int main(int argc, char * argv[]){
 //891868 895662
 
-if(argc == 2 && strcmp(argv[1], "--challenge") == 0){       //cambia comportamento del main se viene 
-    printf("%d\n%s\n", argc, argv[1]);                      //parametro "--challenge" in esecuzione
-    return 0;                                               //qui ora al posto della printf(test)
-                                                            //basta inserire la funzione che gestisce la IA
-}
-    int row = 10;
-    int col = 19;
-    char **M = (char**) malloc(row * sizeof(char*));
 
+    int row, col;
+    scanf("%d\n%d\n", &col, &row);
+    
+    char ** M = (char**)malloc(row * sizeof(char*));     //alloca la matrice
     for(int i = 0; i < col; i++){
-        M[i] = (char*) malloc(col * sizeof(char));  //alloca matrice
+        M[i] = (char*)malloc(col * sizeof(char));
     }
 
 
+    if(argc == 2 && strcmp(argv[1], "--challenge") == 0){           //--challenge branch (IA)
 
-    labyrinth_initializer(M);                //inizializza la matrice labirinto
-    labyrinth_player(M, &row, &col);         //modifica la matrice facendo muovere il giocatore
+        matrix_reader(M, &row, &col);
+
+        for(int i = 0; i < row; i++){                   //print per test, cancellabile
+                for(int j = 0; j < col; j++){      
+                        printf("%c", M[i][j]);
+                }
+                printf("\n");
+            }   
+        printf("\n");
+
+
+
+
+
+        return 0;
+    }else{
+        //labyrinth_initializer(M);
+        matrix_reader(M, &row, &col);
+        labyrinth_player(M, &row, &col);                       //modifica la matrice facendo muovere il giocatore
+        return 0;
+    }
     return 0;
 }
+
+
+    
